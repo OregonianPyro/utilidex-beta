4168122debdc41aecd679cdb3f7a5b85976db045
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-08-29 21:59:40 -0700

    auto-unmutes any muted user upon restart

--- events/ready.js
+++ events/ready.js
@@ -10,6 +10,18 @@ class Ready {
         });
         const time = this.client.moment().format('dddd, MMMM Do, YYYY, hh:mm:ss A');
         console.log(this.chalk.bgBlack.greenBright.inverse(`[ READY ] ${this.client.user.tag} has connected on ${time}\``));
+        this.client.ready = true;
+        //unmutes any muted user on restart, so they aren't stuck muted.
+        this.client.guilds.forEach(g => {
+            this.client.mutes.delete(g.id);
+            this.client.mutes.set(g.id, []);
+            g.members.forEach(m => {
+                const { mute_role } = this.client.settings.get(g.id);
+                if (!m.roles.has(mute_role)) return;
+                m.removeRole(mute_role, 'Bot Restarted - Removing Muted Role from User.');
+                console.log(this.chalk.bgGreen(`Successfully Removed the Muted Role for ${m.user.tag} in '${g.name}'`));
+            });
+        });
     };
 };
 
