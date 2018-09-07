5befcd2673466d7dc5fb4c5d52462f14049244e5
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-09-06 22:07:42 -0700

    finally a fun command

---
+++ commands/fun/ascii.js
@@ -0,0 +1,32 @@
+const Command = require('../../base/command.js');
+const figlet = require('figlet');
+
+class Ascii extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ascii',
+            category: 'fun',
+            description: 'Sends ascii text, fancy right?',
+            usage: '{prefix}ascii <text>',
+            parameters: 'stringText',
+            extended: false,
+            enabled: true,
+            reason: null,
+            devOnly: false,
+            guildOnly: false,
+            permission: 'SEND_MESSAGES',
+            bot_permission: 'SEND_MESSAGES',
+            aliases: []
+        });
+    };
+
+    async run(message, args) {
+        if (!args[0]) return this.client.help(this.client, message, 'ascii');
+        figlet(args.join(' '), (err, data) => {
+            if (err) throw new Error(err.message), console.error(err.stack);
+            return message.channel.send(data, { code: 'ascii' }), console.log(data); 
+        });
+    };
+};
+
+module.exports = Ascii;
